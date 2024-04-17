874b3ab6ac4ddd15f2eae027bb4364b8751dff07
Hritik <pypi.pioneer@gmail.com>
2024-04-17 14:35:04 +0530

    user registered successfully

--- src/components/forms/RegistrationForm.jsx
+++ src/components/forms/RegistrationForm.jsx
@@ -1,5 +1,9 @@
 import React, { useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
 import '../../App.css';
+import { registerUser } from '../../services/user';
+import LoggingUser from '../loader/LoggingUser';
 
 
 export default function RegistrationForm() {
@@ -10,10 +14,34 @@ export default function RegistrationForm() {
     const contactRef = useRef(null);
     const passwordRef = useRef(null);
 
+    // hooks to call actions and navigate
+    // const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { isLoading } = useSelector(state => state.user);
+
     // to handle the form
     const handleFormSubmit = (e) => {
         e.preventDefault();
-        console.log(nameRef.current.value)
+
+        const formData = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            contactNumber: contactRef.current.value,
+            password: passwordRef.current.value,
+        };
+
+        // now, submit the data and dispatch the action
+        dispatch(registerUser(formData))
+            .then(status => {
+
+                // if user created successfully
+                if (status.type === 'registerUser/fulfilled') {
+                    setTimeout(() => {
+                        // navigate('/');
+                    }, 0);
+                }
+            })
+            .catch();
     }
 
     return (
@@ -24,7 +52,7 @@ export default function RegistrationForm() {
                     <div className="card card-design">
                         <div className="card-body">
                             <h5 className="card-title text-center mb-4 font-weight-bold">Registration Form</h5>
-                            
+
                             <form onSubmit={handleFormSubmit}>
                                 <div className="mb-3">
                                     <input ref={nameRef} id="form-input" type="text" className="form-control form-input" placeholder="Full Name" autoComplete="off" required style={{ backgroundColor: 'transparent', border: 'none', outline: 'none', resize: 'none', boxShadow: 'none', borderBottom: '1px solid black', color: 'black' }} />
@@ -41,7 +69,7 @@ export default function RegistrationForm() {
                                 <button type="submit" className="btn btn-primary w-100">Register</button>
                             </form>
 
-                            <p className="text-center mt-3">Already have an account? <a href="/login">Login</a></p>
+                            {isLoading ? <LoggingUser /> : <p className="text-center mt-3">Already have an account? <a href="/login">Login</a></p>}
                         </div>
                     </div>
 
